8a373a013d575375ee408b8ae3e6606d6fc4c469
nagarkamakshi <nagarkamakshi@users.noreply.github.com>
2020-02-21 11:53:04 -0800

    added Landscaper and DeliveryDron tests for more coverage

--- test/SmackDat2/DeliveryDroneTest.java
+++ test/SmackDat2/DeliveryDroneTest.java
@@ -1,9 +1,6 @@
 package SmackDat2;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-import battlecode.common.Team;
+import battlecode.common.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,14 +26,46 @@ public class DeliveryDroneTest {
         when(rc.isCurrentlyHoldingUnit()).thenReturn(true);
         ddTest.takeTurn();
     }
+    /*public void runDeliveryDrone() throws GameActionException {
+        Team enemy = rc.getTeam().opponent();
 
+        if (rc.isCurrentlyHoldingUnit()) {
+            nav.tryMove(Util.randomDirection());
+        } else if (rc.isReady()) {
+            RobotInfo[] robots = rc.senseNearbyRobots();
+            if (robots.length == 0) {
+                MapLocation m = getHQLocation();
+                rc.move(rc.getLocation().directionTo(m).opposite());
+            }
+            for (RobotInfo r : robots) {
+                if (r.team == enemy) {
+                    if (rc.getLocation().isAdjacentTo(r.getLocation())) {
+                        if (rc.canPickUpUnit(r.getID())) {
+                            rc.pickUpUnit(r.getID());
+                            break;
+                        }
+                    }
+                }
+            }
+            //move to the enemy direction
+            for (RobotInfo r : robots) {
+                if (r.team == enemy) {
+                    rc.move(rc.getLocation().directionTo(r.getLocation()));
+                }
+            }
+        }
+    }*/
     @Test
     public void takeTurnDoNothing() throws GameActionException {
-        RobotInfo[] ri = {mock(RobotInfo.class)};
+        RobotInfo[] ri = new RobotInfo[]{new RobotInfo(12, Team.B, RobotType.HQ, 0, false, 0, 0, 0, new MapLocation(5, 5))};
         when(rc.senseNearbyRobots()).thenReturn(ri);
-        when(rc.getTeam()).thenReturn(Team.A);
+        when(rc.getTeam()).thenReturn(Team.B);
         when(rc.isCurrentlyHoldingUnit()).thenReturn(false);
-        when(rc.isReady()).thenReturn(false);
+        when(rc.isReady()).thenReturn(true);
+        when(rc.senseNearbyRobots()).thenReturn(ri);
+        //when(rc.getLocation().isAdjacentTo(any())).thenReturn(true);
+        //assertEquals(ri.length,0);
+
         ddTest.takeTurn();
     }
 
