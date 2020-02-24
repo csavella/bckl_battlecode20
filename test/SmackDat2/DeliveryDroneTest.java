07051622dbb81a9ded3966ad30a7038fc5496493
Brandon T <TishenB@users.noreply.github.com>
2020-02-23 19:44:31 -0800

    testfix

--- test/SmackDat2/DeliveryDroneTest.java
+++ test/SmackDat2/DeliveryDroneTest.java
@@ -1,9 +1,6 @@
 package SmackDat2;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
-import battlecode.common.Team;
+import battlecode.common.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,6 +24,7 @@ public class DeliveryDroneTest {
         when(rc.senseNearbyRobots()).thenReturn(ri);
         when(rc.getTeam()).thenReturn(Team.A);
         when(rc.isCurrentlyHoldingUnit()).thenReturn(true);
+        when(rc.getLocation()).thenReturn(new MapLocation(2, 2));
         ddTest.takeTurn();
     }
 
