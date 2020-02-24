4fb749119b9865fe97bf3b9e4817723034e87e77
Brandon T <TishenB@users.noreply.github.com>
2020-02-23 19:08:59 -0800

    HQ has netgun again

--- src/SmackDat2/HQ.java
+++ src/SmackDat2/HQ.java
@@ -1,9 +1,6 @@
 package SmackDat2;
 
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
+import battlecode.common.*;
 
 public class HQ extends Shooter {
     static int numMiners = 0;
@@ -17,6 +14,18 @@ public class HQ extends Shooter {
         if(turnCount == 1) {
             comms.sendHqLoc(rc.getLocation());
         }
+
+        //Sense all robots near the HQ, if there is a drone from the enemy team in the radius,
+        //Shoot it down
+        Team myTeamColor = rc.getTeam();
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
+        for (RobotInfo r : nearbyRobots) {
+            // TEST: System.out.println("Robot nearby");
+            if (r.team != myTeamColor && rc.canShootUnit(r.ID))
+                rc.shootUnit(r.ID);
+
+        }
+        
         if(numMiners < 3) {
             for (Direction dir : Util.directions)
                 if(tryBuild(RobotType.MINER, dir)){
